b3d4eb6afe7415035e3e2188aa15d453762c319d
keewonf <lukasfmm@gmail.com>
2026-01-20 11:06:29 -0300

    feat: update schedules list on submit

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 
 import { scheduleNew } from "../../services/schedule-new.js";
+import { schedulesDay } from "../schedules/load.js";
 
 const form = document.querySelector("form");
 const clientName = document.getElementById("client");
@@ -31,6 +32,10 @@ form.addEventListener("submit", async (e) => {
     const id = new Date().getTime();
 
     await scheduleNew({ id, name, when });
+
+    await schedulesDay();
+
+    clientName.value = "";
   } catch (error) {
     alert("Não foi possível realizar o agendamento.");
     console.log(error);
