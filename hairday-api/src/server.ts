c401466a2a1588e5dbd83017d4c9ef0c9bd1a2c4
keewonf <lukasfmm@gmail.com>
2026-02-23 19:17:31 -0300

    feat(api): add AppError class and global error handler

--- hairday-api/src/server.ts
+++ hairday-api/src/server.ts
@@ -1,9 +1,30 @@
 import express from "express";
+import type { Request, Response, NextFunction } from "express";
+import { routes } from "./routes/index.js";
+import { AppError } from "./utils/app-error.js";
+import { z, ZodError } from "zod";
 
 const PORT = 3333;
 
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
+
+app.use(routes);
+
+app.use((error: any, req: Request, res: Response, _: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
+  if (error instanceof ZodError) {
+    return res
+      .status(400)
+      .json({ message: "Validation error!", issues: z.treeifyError(error) });
+  }
+
+  return res.status(500).json({ message: error.message });
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
