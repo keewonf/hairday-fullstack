7b286f46f14b8439cdf5b8c27dab876f06a7e70b
keewonf <lukasfmm@gmail.com>
2026-02-23 19:18:14 -0300

    feat(api): implement schedules controller using repository; add validation and create/delete behaviour

--- hairday-api/src/controllers/schedules-controllers.ts
+++ hairday-api/src/controllers/schedules-controllers.ts
@@ -1,13 +1,27 @@
 import type { Request, Response } from "express";
+import { SchedulesRepository } from "../repositories/schedules-repository.js";
 import z from "zod";
+import { AppError } from "../utils/app-error.js";
 
 class SchedulesController {
-  index(req: Request, res: Response) {
+  private schedulesRepository = new SchedulesRepository();
+  index = (req: Request, res: Response) => {
     const { date } = req.query;
-    res.send(date);
-  }
 
-  create(req: Request, res: Response) {
+    let schedules;
+
+    if (!date) {
+      schedules = this.schedulesRepository.findAll();
+    } else if (typeof date === "string") {
+      schedules = this.schedulesRepository.findByDate(date);
+    } else {
+      throw new AppError("Data inválida");
+    }
+
+    return res.json(schedules);
+  };
+
+  create = (req: Request, res: Response) => {
     const bodySchema = z.object({
       name: z.string().trim().min(1, "Name must have at least 1 character"),
       when: z.iso.datetime({
@@ -17,14 +31,28 @@ class SchedulesController {
 
     const { name, when } = bodySchema.parse(req.body);
 
-    res
-      .status(201)
-      .json(`Parabéns ${name} seu agendamento foi registrado na data ${when}`);
-  }
+    const schedule = {
+      id: crypto.randomUUID(),
+      name,
+      when: new Date(when),
+    };
 
-  remove(req: Request, res: Response) {
-    res.status(200).json(`Seu produto foi removido`);
-  }
+    this.schedulesRepository.create(schedule);
+
+    return res.status(201).json(schedule);
+  };
+
+  remove = (req: Request, res: Response) => {
+    const paramsSchema = z.object({
+      id: z.uuid(),
+    });
+
+    const { id } = paramsSchema.parse(req.params);
+
+    this.schedulesRepository.delete(id);
+
+    return res.status(204).send();
+  };
 }
 
 export { SchedulesController };
